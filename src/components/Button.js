e4c9b117a12f2c753954635765afb416673e937b
Thomas Rodrigues Bragança Cezar <thomasrbcezar@gmail.com>
2024-05-10 18:05:26 -0300

    changed the style

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 // Button component receiving props for label, onClick handler, and id
 const Button = ({ label, onClick, id }) => {
   return (
-    <button className="btn btn-secondary" id={id} onClick={onClick}>
+    <button className="btn btn-secondary " id={id} onClick={onClick}>
       {label}
     </button>
   );
