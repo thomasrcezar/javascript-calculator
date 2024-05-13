08b589d99f752219e85d60422c08296d00f1de18
Thomas Rodrigues Bragança Cezar <thomasrbcezar@gmail.com>
2024-05-13 17:12:21 -0300

    fixing bugs in the code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Update this import
 import './index.css';
-import App from './App';
+import App from './app.js'; // Update this import
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Create root
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
